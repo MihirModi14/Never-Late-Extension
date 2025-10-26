bc7f97bdef40816e93e3c554ff40b61b96766070
MihirModi <mihir.modi@smartsensesolutions.com>
2025-10-26 16:35:56 +0530

    feat: info logger added

--- src/background/alarm.ts
+++ src/background/alarm.ts
@@ -4,6 +4,6 @@ import { logger } from "../helpers/logger.helper";
 chrome.alarms.create("fetchEvents", { periodInMinutes: 30 });
 
 chrome.alarms.onAlarm.addListener((alarm) => {
-  logger.log("fetching meetings", new Date().getTime());
+  logger.info("fetching meetings", new Date().getTime());
   if (alarm.name === "fetchEvents") getCalendarEventsApi();
 });
--- src/helpers/logger.helper.ts
+++ src/helpers/logger.helper.ts
@@ -6,6 +6,7 @@ const colors = {
   FgGreen: "\x1b[32m",
   FgYellow: "\x1b[33m",
   FgRed: "\x1b[31m",
+  FgBlue: "\x1b[34m",
 };
 
 export const logger = {
@@ -14,22 +15,25 @@ export const logger = {
 
   log(...args: unknown[]) {
     console.log(
-      `${colors.FgGreen}[INFO]${
-        colors.Reset
+      `${colors.FgGreen}[INFO]${colors.Reset
+      } [${this.currentDateTime()}]: ${args.join(" ")}`
+    );
+  },
+  info(...args: unknown[]) {
+    console.log(
+      `${colors.FgBlue}[INFO]${colors.Reset
       } [${this.currentDateTime()}]: ${args.join(" ")}`
     );
   },
   warn(...args: unknown[]) {
     console.log(
-      `${colors.FgYellow}[WARN]${
-        colors.Reset
+      `${colors.FgYellow}[WARN]${colors.Reset
       } [${this.currentDateTime()}]: ${args.join(" ")}`
     );
   },
   error(...args: unknown[]) {
     console.log(
-      `${colors.FgRed}[ERROR]${
-        colors.Reset
+      `${colors.FgRed}[ERROR]${colors.Reset
       } [${this.currentDateTime()}]: ${args.join(" ")}`
     );
   },
