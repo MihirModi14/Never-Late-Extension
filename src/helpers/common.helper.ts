2d115e187273c5f33816545c4bc2c35a09442f31
MihirModi <mihir.modi@smartsensesolutions.com>
2025-10-26 16:41:06 +0530

    refactor: node forge package remove

--- src/helpers/common.helper.ts
+++ src/helpers/common.helper.ts
@@ -1,5 +1,3 @@
-import forge from "node-forge";
-
 export const isEmptyValue = (value: unknown) => {
   if (value === null || value === undefined) {
     return true;
@@ -14,16 +12,4 @@ export const isEmptyValue = (value: unknown) => {
     return true;
   }
   return false;
-};
-
-export const ENCRYPT = (value: string | null): string | null => {
-  return value
-    ? forge.util.encode64(forge.util.encodeUtf8(JSON.stringify(value)))
-    : null;
-};
-
-export const DECRYPT = <T>(value: string | null): T | null => {
-  return value
-    ? JSON.parse(forge.util.decodeUtf8(forge.util.decode64(value.toString())))
-    : null;
 };
